4de8059fb4b5c2c4405fc4d02367b7d41268ebbb
WenGeXiao <1251595041@qq.com>
2022-06-30 22:42:15 +0800

    1.加入用户名不能重复 2.密码加密

--- mall-application/src/main/java/com/wgx/MallApplication.java
+++ mall-application/src/main/java/com/wgx/MallApplication.java
@@ -3,8 +3,9 @@ package com.wgx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.wgx.ums.service", "com.wgx.mall"})
+@SpringBootApplication(scanBasePackages = {"com.wgx.ums.service", "com.wgx.mall"},exclude = {SecurityAutoConfiguration.class})
 @MapperScan("com.wgx.mall.mapper")
 public class MallApplication {
     public static void main(String[] args) {
--- mall-service/mall-service-ums/src/main/java/com/wgx/mall/service/impl/UmsMemberServiceImpl.java
+++ mall-service/mall-service-ums/src/main/java/com/wgx/mall/service/impl/UmsMemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wgx.mall.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wgx.mall.mapper.UmsMemberMapper;
 import com.wgx.ums.entity.dto.UserMemberDTO;
@@ -8,7 +9,9 @@ import com.wgx.ums.entity.po.UmsMember;
 import com.wgx.ums.service.UmsMemberService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
@@ -24,12 +27,62 @@ public class UmsMemberServiceImpl extends ServiceImpl<UmsMemberMapper, UmsMember
     @Autowired
     private UmsMemberMapper umsMemberMapper;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public String register(UserMemberDTO userMemberDTO) {
+        // todo 基本校验
         UmsMember umsMember = new UmsMember();
         BeanUtils.copyProperties(userMemberDTO, umsMember);
+        // 业务校验
+        if(!StringUtils.isEmpty(bussinessValidate(umsMember))){
+            return "用户名重复";
+        }
+        // 密码加密
+        encryPwd(umsMember);
+        // todo: 公共响应
+        // todo 异常处理
         // mybatis_plus返回的是主键id
         int userId = umsMemberMapper.insert(umsMember);
         return "success";
     }
+
+
+    private void encryPwd(UmsMember umsMember) {
+        // 取出密码
+        String pwd = umsMember.getPassword();
+        // 加密
+        umsMember.setPassword(bCryptPasswordEncoder.encode(pwd));
+    }
+
+    private String bussinessValidate(UmsMember umsMember) {
+        if(!isRepeatUserName(umsMember.getUsername())){
+            return "用户名重复";
+        }
+        return null;
+    }
+
+
+    /**
+     * 校验用户名是否重复
+     * 1.查询判断
+     * 2.数据库 username + 唯一索引
+     ******************************************
+     * @author 肖文格 [2022/6/29 23:56]
+     * @version 1.0.0
+     ******************************************
+     * @param username  用户名
+     * @return true:不重复 false 重复
+     */
+    private boolean isRepeatUserName(String username) {
+        // 防止用户名重复
+        // 获取用户名
+        // 根据用户名去查询数据库,如果有返回，则返回用户名重复，否则继续返回true
+        QueryWrapper<UmsMember> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        int count = umsMemberMapper.selectCount(queryWrapper);
+        return count == 0;
+    }
+
 }
